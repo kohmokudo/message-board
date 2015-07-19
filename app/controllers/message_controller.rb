ad3df42e6e3e235b7ba17041773458ab0ffa4271
kohmokudo <kosukeshinozaki@gmail.com>
2015-07-19 12:44:00 +0000

    create message model

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,4 +1,18 @@
 class MessageController < ApplicationController
+  
   def index
+    # Messageを全て取得する。
+    @messages = Message.all
+  end
+  
+  def create
+    @message = Message.new(messsage_params)
+    @message.save
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+    #params.require(:message).permit(:name, :body)
   end
 end
